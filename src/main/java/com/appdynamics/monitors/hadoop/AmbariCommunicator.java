0d92e1192dc8034721f9342a056ebb8ef191a0d7
stephen.dong <stephen.dong@appdynamics.com>
2013-09-24 17:53:11 -0700

    Initial commit

---
+++ src/main/java/com/appdynamics/monitors/hadoop/AmbariCommunicator.java
@@ -0,0 +1,11 @@
+package com.appdynamics.monitors.hadoop;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: stephen.dong
+ * Date: 9/24/13
+ * Time: 4:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AmbariCommunicator {
+}
