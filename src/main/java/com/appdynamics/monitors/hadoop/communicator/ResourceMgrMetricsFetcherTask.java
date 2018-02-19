0bc8e69b4409668e5c8e0a1c70e11b537f20f50f
akshayAppd <akshay.srivastava@appdynamics.com>
2018-02-19 17:29:21 +0530

    v2.5.1 - Updated Licenses

--- src/main/java/com/appdynamics/monitors/hadoop/communicator/ResourceMgrMetricsFetcherTask.java
+++ src/main/java/com/appdynamics/monitors/hadoop/communicator/ResourceMgrMetricsFetcherTask.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2013. AppDynamics LLC and its affiliates.
+ *  * All Rights Reserved.
+ *  * This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *  * The copyright notice above does not evidence any actual or intended publication of such source code.
+ */
+
 package com.appdynamics.monitors.hadoop.communicator;
 
 import com.appdynamics.extensions.NumberUtils;
